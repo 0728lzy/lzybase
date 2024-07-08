c890a99ffba997df355965caeb3ee4e38f547bc2
Daniel Liu <1276223841@qq.com>
2024-07-08 11:23:37 +0800

    commit

--- commonbase/build.gradle.kts
+++ commonbase/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("maven-publish")
 }
 
 android {
@@ -30,6 +31,35 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    tasks.register<Jar>("generateSourcesJar") {
+        from(project.android.sourceSets.getByName("main").java.srcDirs)
+        archiveClassifier.set("sources")
+    }
+    afterEvaluate {
+        publishing {
+            publications {
+                // 创建一个名为 "release" 的 Maven 发布
+                register("release", MavenPublication::class) {
+                    // 应用 release 构建变体的组件
+                    from(components["release"])
+                    // 可以根据需要自定义发布的属性
+                    groupId = "com.lazyliuzy.lzybase"
+                    artifactId = "commonbase"
+                    version = "1.0"
+                }
+
+                // 创建一个名为 "debug" 的 Maven 发布
+                register("debug", MavenPublication::class) {
+                    // 应用 debug 构建变体的组件
+                    from(components["debug"])
+                    groupId = "com.lazyliuzy.lzybase"
+                    artifactId = "commonbase"
+                    version = "1.0"
+                }
+            }
+        }
+    }
+
 }
 
 dependencies {
